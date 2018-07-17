cd73212b2582aca971a73c09bdcb06e780fca567
ProfessorProspector <thedoctorsoda@gmail.com>
2018-07-17 11:58:33 -0700

    Fix build

--- src/main/java/contrivitive/gui/ContrivitiveGuiHandler.java
+++ src/main/java/contrivitive/gui/ContrivitiveGuiHandler.java
@@ -1,6 +1,5 @@
 package contrivitive.gui;
 
-import contrivitive.GuiManual;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
@@ -20,9 +19,6 @@ public class ContrivitiveGuiHandler implements IGuiHandler {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
-		if (id == 0) {
-			return new GuiManual();
-		}
 		return null;
 	}
 }
